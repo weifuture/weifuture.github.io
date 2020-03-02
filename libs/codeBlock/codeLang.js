4f8443926cfcd8010d60443a63019b481ae3fed1
hongweifuture <hongweifuture@126.com>
2020-03-03 01:26:48 +0800

    Update main.yml from Github Actions at 2020-03-03 01:26:15

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 03 2020 01:17:27 GMT+0800 (China Standard Time)
+// build time:Tue Mar 03 2020 01:26:45 GMT+0800 (China Standard Time)
 $(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
 //rebuild by neat 
